18f9f460b8fe7d26194e2442e767e0b2c809e13d
judy_her <judith85dm@ucla.edu>
2023-11-17 13:31:38 -0800

    added css display flex to card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 console.log('i am connected :)');
 var timeLeft = document.querySelector('#time-left');
 
-//SECTION setInterval
+//SECTION setInterval-------------------------------------
 var secondsLeft = 20;
 var myInterval = setInterval(setTime, 1000);
 
