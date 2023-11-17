4c8cf6a6e161c3d3cc6ee2948c901d22a2ff0ef5
judy_her <judith85dm@ucla.edu>
2023-11-17 13:09:24 -0800

    added setInterval and id for it on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 console.log('i am connected :)');
+var timeLeft = document.querySelector('#time-left');
+
+//SECTION setInterval
+var secondsLeft = 20;
+var myInterval = setInterval(setTime, 1000);
+
+function setTime() {
+  secondsLeft--;
+  timeLeft.textContent = secondsLeft + ' seconds left';
+  if (secondsLeft === 0) {
+    clearInterval(myInterval);
+  }
+}
+setTime();
