9fbdde066e485ea4d53ea20c6e43e91896338dcd
judy_her <judith85dm@ucla.edu>
2023-11-17 20:45:19 -0800

    added style display none to hide start btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 console.log('i am connected :)');
 var timeLeft = document.querySelector('#time-left');
 var startBtn = document.querySelector('.startbtn');
+var titleText = document.querySelector('.title');
+var titleQuestion = document.querySelector('.title-question');
+var question1 = document.querySelector('.question');
 
 //SECTION setInterval-------------------------------------
 var secondsLeft = 20;
@@ -16,7 +19,15 @@ function setTime() {
 setTime();
 
 //SECTION add event listener to start quiz-------------------
-startBtn.addEventListener('click', function () {
+startBtn.addEventListener('click', function (event) {
+  event.preventDefault();
   //run first question here
-  alert('you pressed the start quiz button');
+  titleText.textContent = '';
+  titleQuestion.textContent = '';
+  //hide start button on click
+  startBtn.style.display = 'none';
 });
+//SECTION make function for start button
+var question1 = function () {
+  titleText.textContent = question1;
+};
