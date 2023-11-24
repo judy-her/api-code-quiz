18aa33802e4f75700c071a5fd30f228811a6a08a
judy_her <judith85dm@ucla.edu>
2023-11-24 13:34:55 -0800

    added changes to js.2 script, added currentQuestion ++ after the clear correctWrongMessage

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,4 +1,4 @@
-console.log('i am connected :)');
+console.log('i am script 2 :)');
 var timeLeft = document.querySelector('#time-left');
 var startBtn = document.querySelector('.startbtn');
 var titleText = document.querySelector('.title');
@@ -10,6 +10,7 @@ var countEl = document.querySelector('#score-count');
 
 //SECTION create a function to keep track of score
 function scoreCount() {
+  //   count += 3;
   countEl.textContent = count;
 }
 
@@ -26,6 +27,8 @@ function setTime() {
 }
 
 //SECTION start Quiz function----------------------
+var correctAnswerBtn;
+var currentQuestion = 1;
 function startQuiz() {
   //hide start button on click
   startBtn.style.display = 'none';
@@ -35,10 +38,31 @@ function startQuiz() {
   myInterval = setInterval(setTime, 1000);
   //show timer
   setTime();
-  //call question1 here
-  question1();
+  if (currentQuestion === 1) {
+    question1();
+  } else if (currentQuestion === 2) {
+    question2();
+  } else if (currentQuestion === 3) {
+    question3();
+  } else if (currentQuestion === 4) {
+    question4();
+  } else if (currentQuestion === 5) {
+    question5();
+  }
+  //   switch (currentQuestion) {
+  //     case 1:
+  //       question1();
+  //       break;
+  //     case 2:
+  //       question2();
+  //       break;
+  //     case 3:
+  //       question3();
+  //       break;
+  //     default:
+  //       console.log('no matching cases found');
+  //   }
 }
-
 //SECTION add event listener to start quiz-------------------
 startBtn.addEventListener('click', function (event) {
   event.preventDefault();
@@ -48,23 +72,22 @@ startBtn.addEventListener('click', function (event) {
 function clearCorrectWrongMessage() {
   correctWrongAlert.textContent = '';
 }
-
 //SECTION function for question 1------------------
 //NOTE Xpert learning ai helped with current question variable to keep track of current question
-var correctAnswerBtn;
-var currentQuestion = 1;
+// var correctAnswerBtn;
 
 var question1 = function () {
   clearCorrectWrongMessage();
-
+  currentQuestion++;
+  console.log('this is question1 and should say 2 ' + currentQuestion);
   titleText.textContent =
     '1) Which tag do you use in your HTML link your JavaScript? ';
   btnContainer.innerHTML = `
-    <button class="answer one">link</button>
-    <button class="answer two">body</button>
-    <button class="answer three">script</button>
-    <button class="answer four">div</button>
-  `;
+      <button class="answer one">link</button>
+      <button class="answer two">body</button>
+      <button class="answer three">script</button>
+      <button class="answer four">div</button>
+    `;
 
   correctAnswerBtn = btnContainer.querySelector('.three');
 
@@ -77,62 +100,53 @@ var question1 = function () {
       count += 3;
       scoreCount(); // updates score count
       // xpert ai helped with this
-      currentQuestion++;
+
+      //   currentQuestion++;
 
       setTimeout(function () {
         if (currentQuestion === 2) {
           question2();
-        } else if (currentQuestion === 3) {
-          question3();
-        } else if (currentQuestion === 4) {
-          question4();
-        } else if (currentQuestion === 5) {
-          question5();
-        } else {
         }
       }, 1000); // Add a short delay before adding the next question's event listener
     } else {
       correctWrongAlert.textContent = 'Wrong!';
       secondsLeft -= 5;
-      currentQuestion++;
+      //   currentQuestion++;
 
       setTimeout(function () {
         if (currentQuestion === 2) {
           question2();
-        } else if (currentQuestion === 3) {
-          question3();
-        } else if (currentQuestion === 4) {
-          question4();
-        } else if (currentQuestion === 5) {
-          question5();
         }
       }, 1000); // Add a short delay before adding the next question's event listener
     }
   });
 };
-
 //SECTION Question 2---------------------
 var question2 = function () {
   clearCorrectWrongMessage();
+  currentQuestion++;
+  console.log('this is question2 and should say 3 ' + currentQuestion);
+
   titleText.textContent = '2) I can declare a variable using: ';
   btnContainer.innerHTML = `
-  <button class="answer one">var</button>
-  <button class="answer two">let</button>
-  <button class="answer three">const</button>
-  <button class="answer four">all of the above</button>
-  `;
+    <button class="answer one">var</button>
+    <button class="answer two">let</button>
+    <button class="answer three">const</button>
+    <button class="answer four">all of the above</button>
+    `;
 
   correctAnswerBtn = btnContainer.querySelector('.four');
 
-  function question2ClickHandler(event) {
+  btnContainer.addEventListener('click', function question2ClickHandler(event) {
     clickedBtn = event.target;
 
     if (clickedBtn === correctAnswerBtn) {
       correctWrongAlert.textContent = 'Correct!';
       secondsLeft += 5;
       count += 3;
-      scoreCount(); //updates score count
-      currentQuestion++;
+      //   scoreCount(); //updates score count
+
+      //   currentQuestion++;
 
       setTimeout(function () {
         if (currentQuestion === 3) {
@@ -142,65 +156,21 @@ var question2 = function () {
     } else {
       correctWrongAlert.textContent = 'Wrong!';
       secondsLeft -= 5;
-      currentQuestion++;
+      //   currentQuestion++;
+
       setTimeout(function () {
         if (currentQuestion === 3) {
           question3();
         }
       }, 1000);
-      btnContainer.removeEventListener('click', question2ClickHandler); // Remove event listener for question 2
-      btnContainer.addEventListener('click', question3ClickHandler); // Add event listener for question 3
     }
-  }
-  btnContainer.addEventListener('click', question2ClickHandler);
-  console.log('this should be #2: ' + currentQuestion);
+  });
 };
-// //SECTION Question 3---------------------
-// var question3 = function () {
-//   clearCorrectWrongMessage();
-//   titleText.textContent = '3) Strict equality checks for: ';
-//   btnContainer.innerHTML = `
-//   <button class="answer one">Value</button>
-//   <button class="answer two">Value and Type</button>
-//   <button class="answer three">Type</button>
-//   <button class="answer four">None of the above</button>
-//   `;
-
-//   correctAnswerBtn = btnContainer.querySelector('.two');
-
-//   function question3ClickHandler(event) {
-//     clickedBtn = event.target;
-
-//     if (clickedBtn === correctAnswerBtn) {
-//       correctWrongAlert.textContent = 'Correct!';
-//       secondsLeft += 5;
-//       count += 3;
-//       scoreCount(); //updates score count
-//       currentQuestion++;
-
-//       setTimeout(function () {
-//         if (currentQuestion === 4) {
-//           question4();
-//         }
-//       }, 1000);
-//     } else {
-//       correctWrongAlert.textContent = 'Wrong!';
-//       secondsLeft -= 5;
-//       currentQuestion++;
-//       setTimeout(function () {
-//         if (currentQuestion === 4) {
-//           question4();
-//         }
-//       }, 1000);
-//       btnContainer.removeEventListener('click', question3ClickHandler); // Remove event listener for question 3
-//       btnContainer.addEventListener('click', question4ClickHandler); // Add event listener for question 4
-//     }
-//   }
-//   btnContainer.addEventListener('click', question3ClickHandler);
-// };
-//SECTION Question 3-redo---------------------
+//SECTION Question 3---------------------
 var question3 = function () {
   clearCorrectWrongMessage();
+  currentQuestion++;
+  console.log('this is question3 and should say 4 ' + currentQuestion);
   titleText.textContent = '3) Strict equality checks for: ';
   btnContainer.innerHTML = `
   <button class="answer one">Value</button>
@@ -210,25 +180,26 @@ var question3 = function () {
  `;
 
   correctAnswerBtn = btnContainer.querySelector('.two');
+
   btnContainer.addEventListener('click', function question3ClickHandler(event) {
-    var clickedBtn = event.target;
+    clickedBtn = event.target;
 
     if (clickedBtn === correctAnswerBtn) {
       correctWrongAlert.textContent = 'Correct!';
       secondsLeft += 5;
       count += 3;
-      scoreCount();
-      currentQuestion++;
+      //   scoreCount(); //updates score count
+      //   currentQuestion++;
 
       setTimeout(function () {
-        if (currentQuestion === 5) {
-          question5();
+        if (currentQuestion === 4) {
+          question4();
         }
       }, 1000);
     } else {
       correctWrongAlert.textContent = 'Wrong!';
       secondsLeft -= 5;
-      currentQuestion++;
+      //   currentQuestion++;
 
       setTimeout(function () {
         if (currentQuestion === 4) {
@@ -237,19 +208,20 @@ var question3 = function () {
       }, 1000);
     }
   });
-  console.log('this should be #3: ' + currentQuestion);
 };
 
 //SECTION Question 4---------------------
 var question4 = function () {
   clearCorrectWrongMessage();
+  currentQuestion++;
+  console.log('this is question4 and should say 5 ' + currentQuestion);
   titleText.textContent = '4) What does the JSON.stringify() method do? ';
   btnContainer.innerHTML = `
-  <button class="answer one json-btn">Parse JSON string</button>
-  <button class="answer b json-btn">Convert a JS object to a JSON string</button>
-  <button class="answer three json-btn">Makes string more readable</button>
-  <button class="answer four json-btn">Convert a JSON string to a JS object</button>
- `;
+    <button class="answer one json-btn">Parse JSON string</button>
+    <button class="answer b json-btn">Convert a JS object to a JSON string</button>
+    <button class="answer three json-btn">Makes string more readable</button>
+    <button class="answer four json-btn">Convert a JSON string to a JS object</button>
+   `;
 
   correctAnswerBtn = btnContainer.querySelector('.b');
   btnContainer.addEventListener('click', function question4ClickHandler(event) {
@@ -259,8 +231,7 @@ var question4 = function () {
       correctWrongAlert.textContent = 'Correct!';
       secondsLeft += 5;
       count += 3;
-      scoreCount();
-      currentQuestion++;
+      //   scoreCount();
 
       setTimeout(function () {
         if (currentQuestion === 5) {
@@ -270,7 +241,6 @@ var question4 = function () {
     } else {
       correctWrongAlert.textContent = 'Wrong!';
       secondsLeft -= 5;
-      currentQuestion++;
 
       setTimeout(function () {
         if (currentQuestion === 5) {
@@ -279,18 +249,17 @@ var question4 = function () {
       }, 1000);
     }
   });
-  console.log('this should be #4: ' + currentQuestion);
 };
 //SECTION Question 5---------------------
 var question5 = function () {
   clearCorrectWrongMessage();
   titleText.textContent = '5) What does DOM stand for? ';
   btnContainer.innerHTML = `
-  <button class="answer one">Document Object Model</button>
-  <button class="answer two">Data On Modal</button>
-  <button class="answer three">Design Oriented Model</button>
-  <button class="answer four">Design Object Model</button>
- `;
+    <button class="answer one">Document Object Model</button>
+    <button class="answer two">Data On Modal</button>
+    <button class="answer three">Design Oriented Model</button>
+    <button class="answer four">Design Object Model</button>
+   `;
 
   correctAnswerBtn = btnContainer.querySelector('.one');
   btnContainer.addEventListener('click', function question5ClickHandler(event) {
@@ -300,7 +269,7 @@ var question5 = function () {
       correctWrongAlert.textContent = 'Correct!';
       secondsLeft += 5;
       count += 3;
-      scoreCount(); //updates score count
+      // scoreCount(); //updates score count
     } else {
       correctWrongAlert.textContent = 'Wrong!';
       secondsLeft -= 5;
@@ -311,10 +280,3 @@ var question5 = function () {
     }
   });
 };
-
-function endQuiz() {
-  clearCorrectWrongMessage();
-  //display final score and add initials
-  //1)
-  titleText.textContent = 'Quiz Complete!';
-}
